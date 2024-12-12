71958fd6ab216ab9b29fae9a005d886c943dac3c
ddu2 <flskhhdf@naver.com>
2024-12-12 10:25:50 +0900

    Feat : 커미션 수정 요정 기능 추가

--- src/main/java/com/promptoven/commissionservice/application/CommissionService.java
+++ src/main/java/com/promptoven/commissionservice/application/CommissionService.java
@@ -2,6 +2,7 @@ package com.promptoven.commissionservice.application;
 
 import com.promptoven.commissionservice.domain.CommissionStatus;
 import com.promptoven.commissionservice.dto.in.CreateCommissionRequestDto;
+import com.promptoven.commissionservice.dto.in.RequestModifyReqDto;
 import com.promptoven.commissionservice.dto.out.CommissionListResponseDto;
 import com.promptoven.commissionservice.dto.out.CommissionResponseDto;
 import java.util.List;
@@ -15,4 +16,6 @@ public interface CommissionService {
     List<CommissionListResponseDto> getCommissionList(String userUuid, String sortBy);
 
     void updateCommissionStatus(String commissionUuid, CommissionStatus status);
+
+    void requestCommissionModify(RequestModifyReqDto requestModifyReqDto);
 }
--- src/main/java/com/promptoven/commissionservice/vo/mapper/CommissionVoMapper.java
+++ src/main/java/com/promptoven/commissionservice/vo/mapper/CommissionVoMapper.java
@@ -1,7 +1,9 @@
 package com.promptoven.commissionservice.vo.mapper;
 
 import com.promptoven.commissionservice.dto.in.CreateCommissionRequestDto;
+import com.promptoven.commissionservice.dto.in.RequestModifyReqDto;
 import com.promptoven.commissionservice.vo.in.CreateCommissionRequestVo;
+import com.promptoven.commissionservice.vo.in.RequestModifyReqVo;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +19,13 @@ public class CommissionVoMapper {
                 .commissionModel(createCommissionRequestVo.getCommissionModel())
                 .commissionRequest(createCommissionRequestVo.getCommissionRequest())
                 .commissionModifyRequest(createCommissionRequestVo.getCommissionModifyRequest())
+                .build();
+    }
+
+    public RequestModifyReqDto toRequestModifyReqDto(RequestModifyReqVo requestModifyReqVo) {
+        return RequestModifyReqDto.builder()
+                .commissionUuid(requestModifyReqVo.getCommissionUuid())
+                .commissionModifyRequest(requestModifyReqVo.getCommissionModifyRequest())
                 .build();
     }
 }
